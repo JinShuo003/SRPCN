9dbf657eb74e0cc41368199adb8aea35e6c31b2e
JinShuo003 <710083000@qq.com>
2023-12-21 15:39:41 +0800

    feat: 重写logger获取

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -4,40 +4,20 @@ import os.path
 from utils import path_utils
 
 
-def get_logger(log_options: dict):
-    logger = logging.getLogger(log_options.get("logger_name"))
-    logger.setLevel(level=logging.getLevelName(log_options.get("logger_level")))
+def get_logger(log_dir: str, scene: str):
+    _logger = logging.getLogger()
+    _logger.setLevel("INFO")
 
-    if log_options.get("out_to_file"):
-        file_log_options = log_options.get("file_log_options")
-        path_utils.generate_path(os.path.split(file_log_options.get("file_path"))[0])
-        file_handler = logging.FileHandler(file_log_options.get("file_path"), mode="w")
-        file_handler.setLevel(level=file_log_options.get("log_level"))
-        if file_log_options.get("is_need_formatter"):
-            formatter = logging.Formatter(file_log_options.get("formatter"))
-            file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+    path_utils.generate_path(log_dir)
+    log_filename = "{}.log".format(scene)
+    log_path = os.path.join(log_dir, log_filename)
 
-    if log_options.get("out_to_console"):
-        console_log_options = log_options.get("console_log_options")
-        stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(level=console_log_options.get("log_level"))
-        if console_log_options.get("is_need_formatter"):
-            formatter = logging.Formatter(file_log_options.get("formatter"))
-            stream_handler.setFormatter(formatter)
-        logger.addHandler(stream_handler)
-
-    return logger
-
-
-def add_file_handler(logger, log_path, log_file_name):
-    if not os.path.exists(log_path):
-        os.makedirs(log_path)
-    file_handler = logging.FileHandler(os.path.join(log_path, log_file_name), mode="w")
+    file_handler = logging.FileHandler(log_path.format(scene), mode="w")
     file_handler.setLevel(level=logging.INFO)
-    logger.addHandler(file_handler)
-    return file_handler
+    _logger.addHandler(file_handler)
 
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(level=logging.INFO)
+    _logger.addHandler(stream_handler)
 
-def remove_file_handler(logger, file_handler):
-    logger.removeHandler(file_handler)
+    return _logger, file_handler, stream_handler

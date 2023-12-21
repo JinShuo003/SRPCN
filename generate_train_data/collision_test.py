ff6e68f248f5a717140b8e8e1081a4f7d3ca68bd
JinShuo003 <710083000@qq.com>
2023-12-21 17:14:50 +0800

    feat: 增加log目录、是否使用进程池、进程个数的配置 feat: 新增ibs与Mesh的碰撞检测

--- generate_train_data/collision_test.py
+++ generate_train_data/collision_test.py
@@ -10,7 +10,7 @@ import numpy as np
 import open3d as o3d
 import trimesh.collision
 
-from utils import geometry_utils, path_utils, ibs_utils
+from utils import geometry_utils, path_utils, log_utils
 
 
 def save_ibs_mesh(specs, scene, ibs_mesh_o3d):
@@ -30,12 +30,10 @@ class CollisionAnalyzer:
         self.specs = specs
         self.logger = logger
 
-    def handle_scene(self, scene):
+    def collision_test_origin_mesh(self, scene):
         geometries_path = path_utils.get_geometries_path(self.specs, scene)
         mesh1 = geometry_utils.read_mesh(geometries_path["mesh1"])
         mesh2 = geometry_utils.read_mesh(geometries_path["mesh2"])
-        mesh1.paint_uniform_color((0, 0, 1))
-        mesh2.paint_uniform_color((0, 1, 0))
 
         collision_manager = trimesh.collision.CollisionManager()
         collision_manager.add_object('obj1', geometry_utils.o3d2trimesh(mesh1))
@@ -43,37 +41,34 @@ class CollisionAnalyzer:
         is_collision, data = collision_manager.in_collision_internal(return_data=True)
         if is_collision:
             self.logger.info("collision occured, amount: {}".format(len(data)))
-            collision_points = []
-            for collision_data in data:
-                collision_points.append(collision_data.point)
-            collision_pcd = geometry_utils.get_pcd_from_np(np.array(collision_points))
-            aabb = collision_pcd.get_axis_aligned_bounding_box()
-            aabb.color = (1, 0, 0)
-            o3d.visualization.draw_geometries([aabb, mesh1, mesh2], mesh_show_wireframe=True, mesh_show_back_face=True)
-
         else:
             self.logger.info("No collision")
 
+    def collision_test_ibs(self, scene):
+        geometries_path = path_utils.get_geometries_path(self.specs, scene)
+        mesh1 = geometry_utils.read_mesh(geometries_path["mesh1"])
+        mesh2 = geometry_utils.read_mesh(geometries_path["mesh2"])
+        ibs = geometry_utils.read_mesh(geometries_path["ibs"])
 
-def get_logger(scene: str):
-    _logger = logging.getLogger()
-    _logger.setLevel("INFO")
-    log_path = "logs/collision_test/{}.log"
-    path_utils.generate_path(os.path.split(log_path)[0])
+        collision_manager = trimesh.collision.CollisionManager()
+        collision_manager.add_object('obj1', geometry_utils.o3d2trimesh(mesh1))
+        collision_manager.add_object('obj2', geometry_utils.o3d2trimesh(mesh2))
+        is_collision, data = collision_manager.in_collision_single(geometry_utils.o3d2trimesh(ibs), return_data=True)
+        if is_collision:
+            self.logger.info("collision occured, amount: {}".format(len(data)))
+        else:
+            self.logger.info("No collision")
 
-    file_handler = logging.FileHandler(log_path.format(scene), mode="w")
-    file_handler.setLevel(level=logging.INFO)
-    _logger.addHandler(file_handler)
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(level=logging.INFO)
-    _logger.addHandler(stream_handler)
-
-    return _logger, file_handler, stream_handler
+    def handle_scene(self, scene):
+        self.logger.info("collision test type: {}".format(self.specs.get("type")))
+        if self.specs.get("type") == "origin_mesh":
+            self.collision_test_origin_mesh(scene)
+        elif self.specs.get("type") == "ibs":
+            self.collision_test_ibs(scene)
 
 
 def my_process(scene, specs):
-    _logger, file_handler, stream_handler = get_logger(scene)
+    _logger, file_handler, stream_handler = log_utils.get_logger(specs.get("path_options").get("log_dir"), scene)
     process_name = multiprocessing.current_process().name
     _logger.info(f"Running task in process: {process_name}, scene: {scene}")
     collision_analyzer = CollisionAnalyzer(specs, _logger)
@@ -105,20 +100,23 @@ if __name__ == '__main__':
             for filename in filename_tree[category][scene]:
                 view_list.append(filename)
 
-    pool = multiprocessing.Pool(processes=specs.get("processNum"))
-    for filename in view_list:
-        logger.info("current scene: {}".format(filename))
-        pool.apply_async(my_process, (filename, specs))
-    pool.close()
-    pool.join()
+    if specs.get("use_process_pool"):
+        pool = multiprocessing.Pool(processes=specs.get("process_num"))
 
-    # for filename in view_list:
-    #     logger.info("current scene: {}".format(filename))
-    #     _logger, file_handler, stream_handler = get_logger(filename)
-    #
-    #     collision_analyzer = CollisionAnalyzer(specs, _logger)
-    #     collision_analyzer.handle_scene(filename)
-    #
-    #     _logger.removeHandler(file_handler)
-    #     _logger.removeHandler(stream_handler)
+        for filename in view_list:
+            logger.info("current scene: {}".format(filename))
+            pool.apply_async(my_process, (filename, specs))
+
+        pool.close()
+        pool.join()
+    else:
+        for filename in view_list:
+            logger.info("current scene: {}".format(filename))
+            _logger, file_handler, stream_handler = log_utils.get_logger(specs.get("path_options").get("log_dir"), filename)
+
+            trainDataGenerator = CollisionAnalyzer(specs, _logger)
+            trainDataGenerator.handle_scene(filename)
+
+            _logger.removeHandler(file_handler)
+            _logger.removeHandler(stream_handler)
 

d1e964b96f94de284feafa8232da237a950c5ebf
JinShuo003 <710083000@qq.com>
2024-01-03 13:32:22 +0000

    feat: 修改eval代码

--- train/Transformer_TopNet_MVP/test.py
+++ train/Transformer_TopNet_MVP/test.py
@@ -5,13 +5,16 @@ import os.path
 
 import torch.utils.data as data_utils
 import json
-import open3d as o3d
-import numpy as np
 import re
 import argparse
 import time
+import numpy as np
+import open3d as o3d
+import shutil
 
-from networks.loss import *
+from networks.model_Transformer_TopNet_MVP import *
+from utils.loss import *
+from utils.metric import *
 
 from utils.geometry_utils import get_pcd_from_np
 from utils import log_utils, path_utils
@@ -31,6 +34,7 @@ def get_dataloader(specs):
 
     # get dataset
     test_dataset = dataset_MVP.PcdDataset(data_source, test_split)
+    logger.info("length of test_dataset: {}".format(test_dataset.__len__()))
 
     # get dataloader
     test_dataloader = data_utils.DataLoader(
@@ -40,74 +44,106 @@ def get_dataloader(specs):
         num_workers=num_data_loader_threads,
         drop_last=False,
     )
+    logger.info("length of test_dataloader: {}".format(test_dataloader.__len__()))
 
     return test_dataloader
 
 
-def get_normalize_para(file_path):
-    with open(file_path, "r") as file:
-        content = file.read()
-        data = list(map(float, content.split(",")))
-        translate = data[0:3]
-        scale = data[-1]
-    return translate, scale
-
-
 def save_result(test_dataloader, pcd, indices, specs):
     save_dir = specs.get("ResultSaveDir")
-
-    filename_patten = specs.get("FileNamePatten")
 
     # 将udf数据拆分开，并且转移到cpu
     pcd_np = pcd.cpu().detach().numpy()
 
     filename_list = [test_dataloader.dataset.pcd_partial_filenames[index] for index in indices]
     for index, filename_abs in enumerate(filename_list):
-        # [pcd_partial_2048, scene1, scene1.1000_view0_0]
-        filename_info = filename_abs.split('/')
-        filename = re.match(filename_patten, filename_info[2]).group()
-        category = filename_info[1]
+        # [dataset, category, filename], example:[MVP, scene1, scene1.1000_view0_0.ply]
+        dataset, category, filename = filename_abs.split('/')
 
-        # the real directory is save_dir/category
-        save_path = os.path.join(save_dir, filename_info[0], category)
+        save_path = os.path.join(save_dir, dataset, category)
         if not os.path.isdir(save_path):
             os.makedirs(save_path)
 
-        # append the extend info to the filename
-        filename_final = "{}.ply".format(filename)
-        absolute_dir = os.path.join(save_path, filename_final)
-        pcd = get_pcd_from_np(pcd_np[index])
+        pcd_save_absolute_path = os.path.join(save_path, filename)
 
-        o3d.io.write_point_cloud(absolute_dir, pcd)
+        o3d.io.write_point_cloud(pcd_save_absolute_path, get_pcd_from_np(pcd_np[index]))
 
 
-def test(IBPCDCNet, test_dataloader, specs):
+def create_zip(dataset="MVP"):
+    save_dir = specs.get("ResultSaveDir")
+
+    output_archive = os.path.join(save_dir, dataset)
+
+    shutil.make_archive(output_archive, 'zip', save_dir)
+
+
+def update_loss_dict(dist_dict_total: dict, dist, test_dataloader, indices, tag: str):
+    assert dist.shape[0] == indices.shape[0]
+    assert tag in dist_dict_total.keys()
+
+    dist_dict = dist_dict_total.get(tag)
+    filename_list = [test_dataloader.dataset.pcd_gt_filenames[index] for index in indices]
+    for idx, filename in enumerate(filename_list):
+        category = "scene{}".format(str(int(re.findall(r'\d+', filename)[0])))
+        if category not in dist_dict:
+            dist_dict[category] = {
+                "dist_total": 0,
+                "num": 0
+            }
+        dist_dict[category]["dist_total"] += dist[idx]
+        dist_dict[category]["num"] += 1
+
+
+def cal_avrg_dist(dist_dict_total: dict, tag: str):
+    dist_dict = dist_dict_total.get(tag)
+    dist_total = 0
+    num = 0
+    for i in range(1, 17):
+        category = "scene{}".format(i)
+        dist_dict[category]["avrg_dist"] = dist_dict[category]["dist_total"] / dist_dict[category]["num"]
+        dist_total += dist_dict[category]["dist_total"]
+        num += dist_dict[category]["num"]
+    dist_dict["avrg_dist"] = dist_total/num
+
+
+def test(network, test_dataloader, specs):
     device = specs.get("Device")
 
+    dist_dict = {
+        "cd_l1": {},
+        "cd_l2": {},
+        "emd": {},
+        "fscore": {}
+    }
+    network.eval()
     with torch.no_grad():
-        test_total_loss_emd = 0
-        test_total_loss_cd = 0
         for pcd_partial, pcd_gt, idx in test_dataloader:
             pcd_partial = pcd_partial.to(device)
-            pcd_out = IBPCDCNet(pcd_partial)
-
             pcd_gt = pcd_gt.to(device)
-            loss_emd_pcd = emd_loss(pcd_out, pcd_gt)
-            loss_cd_pcd = cd_loss_L1(pcd_out, pcd_gt)
 
-            batch_loss_emd = loss_emd_pcd
-            batch_loss_cd = loss_cd_pcd
+            pcd_pred = network(pcd_partial)
 
-            test_total_loss_emd += batch_loss_emd.item()
-            test_total_loss_cd += batch_loss_cd.item()
+            cd_l1 = l1_cd(pcd_pred, pcd_gt)
+            cd_l2 = l2_cd(pcd_pred, pcd_gt)
+            emd_ = emd(pcd_pred, pcd_gt)
+            fscore = f_score(pcd_pred, pcd_gt)
 
-            save_result(test_dataloader, pcd_out, idx, specs)
+            update_loss_dict(dist_dict, cd_l1.detach().cpu().numpy(), test_dataloader, idx, "cd_l1")
+            update_loss_dict(dist_dict, cd_l2.detach().cpu().numpy(), test_dataloader, idx, "cd_l2")
+            update_loss_dict(dist_dict, emd_.detach().cpu().numpy(), test_dataloader, idx, "emd")
+            update_loss_dict(dist_dict, fscore.detach().cpu().numpy(), test_dataloader, idx, "fscore")
 
-        test_avrg_loss_emd = test_total_loss_emd / test_dataloader.__len__()
-        logger.info("test_avrg_loss_emd: {}".format(test_avrg_loss_emd))
-        test_avrg_loss_cd = test_total_loss_cd / test_dataloader.__len__()
-        logger.info("test_avrg_loss_cd: {}".format(test_avrg_loss_cd))
+            save_result(test_dataloader, pcd_pred, idx, specs)
+            logger.info("saved {} pcds".format(idx.shape[0]))
 
+        cal_avrg_dist(dist_dict, "cd_l1")
+        cal_avrg_dist(dist_dict, "cd_l2")
+        cal_avrg_dist(dist_dict, "emd")
+        cal_avrg_dist(dist_dict, "fscore")
+
+        logger.info("dist result: \n{}".format(json.dumps(dist_dict, sort_keys=False, indent=4)))
+
+        
 
 def main_function(specs, model_path):
     device = specs.get("Device")
@@ -117,13 +153,20 @@ def main_function(specs, model_path):
     test_dataloader = get_dataloader(specs)
     logger.info("init dataloader succeed")
 
-    IBPCDCNet = torch.load(model_path, map_location="cuda:{}".format(device))
+    model = PCDCNet().to(device)
+    state_dict = torch.load(model_path, map_location="cuda:{}".format(device))
+    model.load_state_dict(state_dict)
     logger.info("load trained model succeed")
 
     time_begin_test = time.time()
-    test(IBPCDCNet, test_dataloader, specs)
+    test(model, test_dataloader, specs)
     time_end_test = time.time()
     logger.info("use {} to test".format(time_end_test - time_begin_test))
+
+    time_begin_zip = time.time()
+    create_zip()
+    time_end_zip = time.time()
+    logger.info("use {} to zip".format(time_end_zip - time_begin_zip))
 
 
 if __name__ == '__main__':
@@ -140,7 +183,7 @@ if __name__ == '__main__':
         "--model",
         "-m",
         dest="model",
-        default="trained_models/Transformer_TopNet_MVP/epoch_45.pth",
+        default="trained_models/Transformer_TopNet_MVP/epoch_90.pth",
         required=False,
         help="The network para"
     )
@@ -153,6 +196,7 @@ if __name__ == '__main__':
 
     logger.info("test split: {}".format(specs.get("TestSplit")))
     logger.info("specs file: {}".format(args.experiment_config_file))
+    logger.info("specs file: \n{}".format(json.dumps(specs, sort_keys=False, indent=4)))
     logger.info("model: {}".format(args.model))
 
     main_function(specs, args.model)

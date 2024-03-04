c816c332334d76313bd1740f9970616f7b751d1b
JinShuo003 <710083000@qq.com>
2024-03-04 15:04:38 +0800

    feat: 添加RBPCDC的训练、测试代码

---
+++ train/RBPCDC_INTE/test.py
@@ -0,0 +1,142 @@
+import sys
+import os
+
+sys.path.insert(0, "/home/data/jinshuo/IBPCDC")
+
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+
+import argparse
+import time
+import os.path
+from datetime import datetime, timedelta
+
+from models.RBPCDC import TopNet_path1, TopNet_path2
+from utils.metric import *
+from utils.test_utils import *
+from utils import log_utils, path_utils, statistics_utils
+from dataset import dataset_RBPCDC
+
+
+def get_network(specs, checkpoint):
+    assert checkpoint is not None
+
+    device = specs.get("Device")
+    logger = LogFactory.get_logger(specs.get("LogOptions"))
+
+    network = model_class(**kwargs).to(device)
+
+    logger.info("load model parameter from epoch {}".format(checkpoint["epoch"]))
+    network.load_state_dict(checkpoint["model"])
+
+    return network
+
+
+def test(network, test_dataloader, specs):
+    device = specs.get("Device")
+
+    dist_dict = {
+        "cd_l1": {},
+        "emd": {},
+        "fscore": {},
+        "mad_s": {},
+        "mad_i": {},
+        "ibs_a": {},
+        "interact_num": {}
+    }
+    single_csv_data = {}
+    network.eval()
+    with torch.no_grad():
+        for data, idx in test_dataloader:
+            filename_list = [test_dataloader.dataset.pcd_partial_filenames[i] for i in idx]
+            center, radius, direction, pcd_partial, pcd_gt = data
+
+            pcd_partial = pcd_partial.to(device)
+            pcd_gt = pcd_gt.to(device)
+            center = center.to(device)
+            radius = radius.to(device)
+            direction = direction.to(device)
+
+            pcd_pred = network(pcd_partial)
+
+            cd_l1 = l1_cd(pcd_pred, pcd_gt)
+            emd_ = emd(pcd_pred, pcd_gt)
+            fscore = f_score(pcd_pred, pcd_gt)
+            mad_s = medial_axis_surface_dist(center, radius, pcd_pred)
+            mad_i = medial_axis_interaction_dist(center, radius, pcd_pred)
+            ibs_a, interact_num = ibs_angle_dist(center, radius, direction, pcd_pred)
+
+            update_loss_dict(dist_dict, filename_list, cd_l1.detach().cpu().numpy(), "cd_l1")
+            update_loss_dict(dist_dict, filename_list, emd_.detach().cpu().numpy(), "emd")
+            update_loss_dict(dist_dict, filename_list, fscore.detach().cpu().numpy(), "fscore")
+            update_loss_dict(dist_dict, filename_list, mad_s.detach().cpu().numpy(), "mad_s")
+            update_loss_dict(dist_dict, filename_list, mad_i.detach().cpu().numpy(), "mad_i")
+            update_loss_dict(dist_dict, filename_list, ibs_a.detach().cpu().numpy(), "ibs_a")
+            update_loss_dict(dist_dict, filename_list, interact_num.detach().cpu().numpy(), "interact_num")
+
+            statistics_utils.append_csv_data(single_csv_data, filename_list, cd_l1, emd_, fscore, mad_s, mad_i, ibs_a, interact_num)
+
+            save_result(filename_list, pcd_pred, specs)
+            logger.info("saved {} pcds".format(idx.shape[0]))
+
+        cal_avrg_dist(dist_dict)
+        logger.info("dist result: \n{}".format(json.dumps(dist_dict, sort_keys=False, indent=4)))
+
+        csv_file_dir = os.path.join(specs.get("ResultSaveDir"), specs.get("TAG"), specs.get("Dataset"))
+        avrg_csv_file_path = os.path.join(csv_file_dir, "avrg_result.csv")
+        statistics_utils.write_avrg_csv_file(avrg_csv_file_path, dist_dict, "INTE")
+
+        single_csv_file_path = os.path.join(csv_file_dir, "single_result.csv")
+        statistics_utils.write_single_csv_file(single_csv_file_path, single_csv_data)
+
+
+def main_function(specs):
+    device = specs.get("Device")
+    model_path = specs.get("ModelPath")
+    logger = log_utils.LogFactory.get_logger(specs.get("LogOptions"))
+    logger.info("test device: {}".format(device))
+    logger.info("batch size: {}".format(specs.get("BatchSize")))
+
+    test_dataloader = get_dataloader(dataset_RBPCDC.RBPCDCDataset, specs)
+    logger.info("init dataloader succeed")
+
+    model = get_network(specs, )
+    network_path1 = get_network(specs, TopNet_path1, None, input_num=2048)
+    network_path2 = get_network(specs, TopNet_path2, None, input_num=2048)
+    checkpoint = torch.load(model_path, map_location="cuda:{}".format(device))
+    model.load_state_dict(checkpoint["model"])
+    logger.info("load trained model succeed, epoch: {}".format(checkpoint["epoch"]))
+
+    time_begin_test = time.time()
+    test(model, test_dataloader, specs)
+    time_end_test = time.time()
+    logger.info("use {} to test".format(time_end_test - time_begin_test))
+
+    time_begin_zip = time.time()
+    create_zip(specs)
+    time_end_zip = time.time()
+    logger.info("use {} to zip".format(time_end_zip - time_begin_zip))
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(description="")
+    arg_parser.add_argument(
+        "--experiment",
+        "-e",
+        dest="experiment_config_file",
+        default="configs/INTE/test/specs_test_TopNet_INTE.json",
+        required=False,
+        help="The experiment config file."
+    )
+    args = arg_parser.parse_args()
+
+    specs = path_utils.read_config(args.experiment_config_file)
+    logger = log_utils.LogFactory.get_logger(specs.get("LogOptions"))
+    
+    TIMESTAMP = "{0:%Y-%m-%d_%H-%M-%S/}".format(datetime.now() + timedelta(hours=8))
+    logger.info("current time: {}".format(TIMESTAMP))
+    logger.info("test split: {}".format(specs.get("TestSplit")))
+    logger.info("specs file: {}".format(args.experiment_config_file))
+    logger.info("specs file: \n{}".format(json.dumps(specs, sort_keys=False, indent=4)))
+    logger.info("model: {}".format(specs.get("ModelPath")))
+
+    main_function(specs)

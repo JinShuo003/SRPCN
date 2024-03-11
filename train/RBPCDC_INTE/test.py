0afb065d6d171c4e71231ea39c736ab9d422e732
JinShuo003 <710083000@qq.com>
2024-03-11 08:38:29 +0000

    bugfix: 修复RBPCDC的测试代码和测试文件

--- train/RBPCDC_INTE/test.py
+++ train/RBPCDC_INTE/test.py
@@ -15,6 +15,27 @@ from utils.metric import *
 from utils.test_utils import *
 from utils import log_utils, path_utils, statistics_utils, geometry_utils
 from dataset import dataset_RBPCDC
+from dataset.dataset_RBPCDC import get_normalize_para_np
+
+
+def get_network(specs, model_class, checkpoint, model_tag, **kwargs):
+    assert checkpoint is not None
+
+    device = specs.get("Device")
+    logger = LogFactory.get_logger(specs.get("LogOptions"))
+
+    network = model_class(**kwargs).to(device)
+
+    logger.info("load model parameter from epoch {}".format(checkpoint["epoch"]))
+    network.load_state_dict(checkpoint[model_tag])
+
+    return network
+
+
+def save_pcd(pcd, path, filename):
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    o3d.io.write_point_cloud(os.path.join(path, filename), pcd)
 
 
 def save_result(filename_list: list, pcd, specs: dict):
@@ -27,38 +48,46 @@ def save_result(filename_list: list, pcd, specs: dict):
     dataset = specs.get("Dataset")
     normalize_para_dir = specs.get("NormalizeParaDir")
 
-    filename_patten = specs.get("FileNamePatten")
+    viewname_patten = specs.get("ViewNamePatten")
     scene_patten = specs.get("ScenePatten")
 
-    pcd1_pred_path1 = pcd1_pred_path1.cpu().detach().numpy()
-    pcd2_pred_path1 = pcd2_pred_path1.cpu().detach().numpy()
-    pcd1_pred_path2 = pcd1_pred_path2.cpu().detach().numpy()
-    pcd2_pred_path2 = pcd2_pred_path2.cpu().detach().numpy()
+    pcd1_pred_path1_batch = pcd1_pred_path1.cpu().detach().numpy()
+    pcd1_pred_path2_batch = pcd1_pred_path2.cpu().detach().numpy()
+    pcd2_pred_path1_batch = pcd2_pred_path1.cpu().detach().numpy()
+    pcd2_pred_path2_batch = pcd2_pred_path2.cpu().detach().numpy()
 
     for index, filename_abs in enumerate(filename_list):
         # [dataset, category, filename], example:[MVP, scene1, scene1.1000_view0_0.ply]
         _, category, filename = filename_abs.split('/')
-        filename = re.match(filename_patten, filename).group()  # scene1.1000_view0_0
+        filename = re.match(viewname_patten, filename).group()  # scene1.1000_view0
+        scene = re.match(scene_patten, filename).group()  # scene1.1000
 
-        # the real directory is save_dir/tag/dataset/category
+        # the save directory is save_dir/tag/dataset/path{0, 1}/category
         path1_save_path = os.path.join(save_dir, tag, dataset, "path1", category)
-        path2_save_path = os.path.join(save_dir, tag, dataset, "path1", category)
-
-        if not os.path.isdir(path1_save_path):
-            os.makedirs(path1_save_path)
-        if not os.path.isdir(path2_save_path):
-            os.makedirs(path2_save_path)
+        path2_save_path = os.path.join(save_dir, tag, dataset, "path2", category)
 
         # get final filename
-        filename_final = "{}.ply".format(filename)
-        absolute_path = os.path.join(save_path, filename_final)
-        pcd = get_pcd_from_np(pcd_np[index])
+        pcd1_filename_final = "{}_0.ply".format(filename)
+        pcd2_filename_final = "{}_1.ply".format(filename)
 
-        # transform to origin coordinate
-        pcd.scale(scale, np.array([0, 0, 0]))
-        pcd.translate(translate)
+        # normalize every geometry
+        pcd1_pred_path1 = get_pcd_from_np(pcd1_pred_path1_batch[index])
+        pcd1_pred_path2 = get_pcd_from_np(pcd1_pred_path2_batch[index])
+        pcd2_pred_path1 = get_pcd_from_np(pcd2_pred_path1_batch[index])
+        pcd2_pred_path2 = get_pcd_from_np(pcd2_pred_path2_batch[index])
 
-        o3d.io.write_point_cloud(absolute_path, pcd)
+        pcd1_translate, pcd1_scale = get_normalize_para_np(os.path.join(normalize_para_dir, category, "{}_{}.txt".format(scene, 0)))
+        pcd2_translate, pcd2_scale = get_normalize_para_np(os.path.join(normalize_para_dir, category, "{}_{}.txt".format(scene, 1)))
+
+        pcd1_pred_path1 = geometry_utils.normalize_geometry(pcd1_pred_path1, pcd1_translate, pcd1_scale)
+        pcd1_pred_path2 = geometry_utils.normalize_geometry(pcd1_pred_path2, pcd1_translate, pcd1_scale)
+        pcd2_pred_path1 = geometry_utils.normalize_geometry(pcd2_pred_path1, pcd2_translate, pcd2_scale)
+        pcd2_pred_path2 = geometry_utils.normalize_geometry(pcd2_pred_path2, pcd2_translate, pcd2_scale)
+
+        save_pcd(pcd1_pred_path1, path1_save_path, pcd1_filename_final)
+        save_pcd(pcd1_pred_path2, path2_save_path, pcd1_filename_final)
+        save_pcd(pcd2_pred_path1, path1_save_path, pcd2_filename_final)
+        save_pcd(pcd2_pred_path2, path2_save_path, pcd2_filename_final)
 
 
 def get_evaluation_metrics(pcd_pred, pcd_gt, pcd_normalize_para, medial_axis_sphere):
@@ -144,7 +173,8 @@ def test(model, test_dataloader, specs):
         "interact_num": {}
     }
     single_csv_data = {}
-    model.eval()
+    network_path1.eval()
+    network_path2.eval()
     with torch.no_grad():
         for data, idx in test_dataloader:
             filename_list = [test_dataloader.dataset.pcd1_partial_filenames[i] for i in idx]
@@ -153,11 +183,10 @@ def test(model, test_dataloader, specs):
             pcd1_partial, pcd2_partial = pcd_partial
             pcd1_gt, pcd2_gt = pcd_gt
 
-            pcd_partial = pcd_partial.to(device)
-            pcd_gt = pcd_gt.to(device)
-            center = center.to(device)
-            radius = radius.to(device)
-            direction = direction.to(device)
+            pcd1_partial = pcd1_partial.to(device).permute(0, 2, 1)
+            pcd2_partial = pcd2_partial.to(device).permute(0, 2, 1)
+            pcd1_gt = pcd1_gt.to(device)
+            pcd2_gt = pcd2_gt.to(device)
 
             pcd_input = torch.concatenate((pcd1_partial, pcd2_partial), dim=2)
             pcd1_pred_path1, pcd2_pred_path1 = network_path1(pcd_input)
@@ -209,11 +238,9 @@ def main_function(specs):
     test_dataloader = get_dataloader(dataset_RBPCDC.RBPCDCDataset, specs)
     logger.info("init dataloader succeed")
 
-    network_path1 = get_network(specs, TopNet_path1, None, input_num=2048)
-    network_path2 = get_network(specs, TopNet_path2, None, input_num=2048)
     checkpoint = torch.load(model_path, map_location="cuda:{}".format(device))
-    network_path1.load_state_dict(checkpoint["model_path1"])
-    network_path2.load_state_dict(checkpoint["model_path2"])
+    network_path1 = get_network(specs, TopNet_path1, checkpoint, "model_path1", input_num=2048)
+    network_path2 = get_network(specs, TopNet_path2, checkpoint, "model_path2", input_num=2048)
     logger.info("load trained model succeed, epoch: {}".format(checkpoint["epoch"]))
 
     time_begin_test = time.time()
@@ -234,7 +261,7 @@ if __name__ == '__main__':
         "--experiment",
         "-e",
         dest="experiment_config_file",
-        default="configs/INTE/test/specs_test_TopNet_INTE.json",
+        default="configs/INTE/test/specs_test_RBPCDC_INTE.json",
         required=False,
         help="The experiment config file."
     )
